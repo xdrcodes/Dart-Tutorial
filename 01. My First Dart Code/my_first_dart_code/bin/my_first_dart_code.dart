2c2f65009e12b58410ba83e134582dbd235091c1
Subham Kr Paul <88024071+Subham137@users.noreply.github.com>
2022-11-08 16:33:28 +0530

    Add files via upload

---
+++ 01. My First Dart Code/my_first_dart_code/bin/my_first_dart_code.dart
@@ -0,0 +1,7 @@
+void main() {
+  print("Hello world");
+  print("This is my first application");
+  print(4);
+  print(12/4);
+  print(true);
+}
---
+++ 01. My First Dart Code/my_first_dart_code/test/my_first_dart_code_test.dart
@@ -0,0 +1,8 @@
+import 'package:my_first_dart_code/my_first_dart_code.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
