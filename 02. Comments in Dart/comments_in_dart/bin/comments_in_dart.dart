36574265f4bfdc03845b1edddc781ddd0dd37c8e
Subham Kr Paul <88024071+Subham137@users.noreply.github.com>
2022-11-09 23:49:18 +0530

    Add files via upload

---
+++ 02. Comments in Dart/comments_in_dart/bin/comments_in_dart.dart
@@ -0,0 +1,20 @@
+/*
+By this method we can 
+write multi line code
+in Dart programming language
+*/
+void main() {
+
+  //This is my first line of code.
+  print("Hello, World!");       // this is another comment ......
+
+  print("This is my first application");
+
+  print(4);
+
+  print(12/4); // Performing arithmetic operator
+
+  print(true); // Printing our boolean value
+
+  print("In this file we have talked about single line and multi line comments.");
+}
---
+++ 02. Comments in Dart/comments_in_dart/test/comments_in_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:comments_in_dart/comments_in_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
