8067ace5568edbaf3d4fb6bb2f35ecfdc1242a3d
Subham Kr Paul <88024071+Subham137@users.noreply.github.com>
2022-11-14 00:11:03 +0530

    Add files via upload

---
+++ 06. If and Else statements/if_and_else_statements/test/if_and_else_statements_test.dart
@@ -0,0 +1,8 @@
+import 'package:if_and_else_statements/if_and_else_statements.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
