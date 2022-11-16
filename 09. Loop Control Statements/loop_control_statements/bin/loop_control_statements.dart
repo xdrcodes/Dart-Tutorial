2a9edb1843b790b189612b6a584c939cffa57dea
Subham Kr Paul <88024071+Subham137@users.noreply.github.com>
2022-11-17 02:19:09 +0530

    Add files via upload

---
+++ 09. Loop Control Statements/loop_control_statements/bin/loop_control_statements.dart
@@ -0,0 +1,22 @@
+void main() {
+  // ITERATORS: SYNTAX
+  
+  //For Loop:
+  for(var i = 0; i < 4; i++) {
+    print("For Loops");
+  }
+  print("");
+  //While Loop:
+  var j = 0;
+  while(j < 4) {
+    print("While Loops");
+    j++;
+  }
+  print("");
+  //Do While Loop:
+  var l = 0;
+  do {
+    print("Do While Loops");
+    l++;
+  } while(l < 4);
+}
---
+++ 09. Loop Control Statements/loop_control_statements/test/loop_control_statements_test.dart
@@ -0,0 +1,8 @@
+import 'package:loop_control_statements/loop_control_statements.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
