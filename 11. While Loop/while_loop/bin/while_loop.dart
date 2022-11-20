fb8afe89a9fb0a52983a1b1efb6b29c53214ddb1
Subham Kr Paul <88024071+Subham137@users.noreply.github.com>
2022-11-20 05:33:45 +0530

    Add files via upload

---
+++ 11. While Loop/while_loop/bin/while_loop.dart
@@ -0,0 +1,11 @@
+void main() {
+  //while Loop
+  //Write a program to find the even numbers 1 to 10
+  var i = 1;
+  while(i <= 10) {
+    if(i % 2 == 0) {
+      print(i);
+    }
+    i++;
+  }
+}
---
+++ 11. While Loop/while_loop/test/while_loop_test.dart
@@ -0,0 +1,8 @@
+import 'package:while_loop/while_loop.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
