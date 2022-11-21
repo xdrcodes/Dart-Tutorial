7cc4d95f4f7795eb21f69726760df530d2d51fd8
Subham Kr Paul <88024071+Subham137@users.noreply.github.com>
2022-11-21 18:16:39 +0530

    Add files via upload

---
+++ 12. Do - While Loop/do_while_loop/bin/do_while_loop.dart
@@ -0,0 +1,12 @@
+void main() {
+  //do-while Loop
+  //Write a program to find odd number 1 to 10
+
+  int i = 1;
+  do {
+    if(i % 2 != 0) {
+      print(i);
+    }
+    i++;
+  } while (i <= 10);
+}
---
+++ 12. Do - While Loop/do_while_loop/test/do_while_loop_test.dart
@@ -0,0 +1,8 @@
+import 'package:do_while_loop/do_while_loop.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
