df8698db172a947e846b68642dc7e58a2ccb3adc
Subham Kr Paul <88024071+Subham137@users.noreply.github.com>
2022-11-22 18:45:31 +0530

    Add files via upload

---
+++ 13. Using 'break' Keyword/break_keyword/bin/break_keyword.dart
@@ -0,0 +1,15 @@
+void main() {
+  //break Keyword
+  //Using Lables
+  //Nested for Loop
+
+  myOuterLoop: for(int i = 1; i <= 3; i++) {
+    for(int j = 1; j <= 3; j++) {
+      print("$i $j");
+
+      if(i == 2 && j == 2) {
+        break myOuterLoop;
+      }
+    }
+  }
+}
---
+++ 13. Using 'break' Keyword/break_keyword/test/break_keyword_test.dart
@@ -0,0 +1,8 @@
+import 'package:break_keyword/break_keyword.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
