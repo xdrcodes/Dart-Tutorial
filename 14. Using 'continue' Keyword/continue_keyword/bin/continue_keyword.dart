6cba25dbac21e4489e4003951a7c9962fe279c1d
Subham Kr Paul <88024071+Subham137@users.noreply.github.com>
2022-11-26 00:11:05 +0530

    Update continue_keyword.dart

--- 14. Using 'continue' Keyword/continue_keyword/bin/continue_keyword.dart
+++ 14. Using 'continue' Keyword/continue_keyword/bin/continue_keyword.dart
@@ -5,7 +5,7 @@ void main() {
   myLoop: for(int i = 1; i <= 3; i++) {
     for(int j = 1; j <= 3; j++) {
       if(i == 2 && j == 2) {
-        continue myLoop;
+        continue myLoop;    // The myLoop in the code is known as Labels in dart and this are used/user defined.
       }
       print("$i $j");
     }
